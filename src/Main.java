5b706cd03510628640b6f0694a4eb5d315e4ed74
Ashwinkumar98 <rpaswin008@gmail.com>
2021-01-25 14:51:35 +0530

    changes

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test Main");
-		System.out.println("Branch 1");
-		if(true) {
+		System.out.println("Branch changes");
+		int x = 1;
+		if(x == 1) {
 			System.out.println("true");
+		}
+		else {
+			System.out.println("Yes");
 		}
 	}
 }
