052a6cbbb6cf7466da1af4a2adb9cfc25625e289
Ashwinkumar98 <rpaswin008@gmail.com>
2021-01-25 15:03:45 +0530

    jhjh

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("ueyavdnmdds");
+		System.out.println("wheh jwegjgew jhjew");
 		int vkk =30;
-		String jdhf = "35t5343";
+		String jdhf = "wejhgj wjgjerw";
 		int x = 1;
 		if(x == 1) {
-			System.out.println("true");
+			System.out.println("wejh hjewjbrw");
 		}
 		else if(x==2) {
 			System.out.println("Yes");
