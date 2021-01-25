195cef61bd8ab750a89279116d803e54de636d18
Ashwinkumar98 <rpaswin008@gmail.com>
2021-01-25 14:56:12 +0530

    9oeue

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Test Main");
 		System.out.println("ueyavdnmdds");
 		int vkk =30;
 		String jdhf = "35t5343";
-		System.out.println("Branch changes");
 		int x = 1;
 		if(x == 1) {
 			System.out.println("true");
